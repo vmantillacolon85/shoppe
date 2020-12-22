fa65f4ce5c85358d89639fe4d4b6b7013c3b2c27
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2020-12-22 11:44:55 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const app = express()
+const db= mongoose.connection
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3003
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.use(express.static("public"))
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(methodOverride("_method"))
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+  console.log("Listening on PORT:", PORT);
+})
