3a399e8078e6fbb1728d6a5970a686769264a851
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2020-12-29 16:31:43 -0500

    editing structure

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+//Dependencies//
+
 const express = require("express")
 const methodOverride = require("method-override")
 const mongoose = require("mongoose")
+
+//generating the app with express//
 const app = express()
+
+//establishing our db with mongoose//
 const db= mongoose.connection
+
+//requiring dotenv for configuration//
 require("dotenv").config()
 
+//estabing our listening PORT variables//
 const PORT = process.env.PORT || 3005
 
+//connecting to mongoose//
 const MONGODB_URI = process.env.MONGODB_URI
 
 mongoose.connect(MONGODB_URI, {
@@ -15,21 +25,25 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
 })
 
-
+//Error Handlers//
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+//Middleware & Body Parser//
 app.use(express.static("public"))
-
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(methodOverride("_method"))
 
+
+//Routes//
 app.get("/", (req, res) => {
   res.send("Hello World you know it")
 })
 
+
+//Listener///
 app.listen(PORT, () => {
   console.log("Listening on PORT:", PORT);
 })
