4e5b6951aad3b45dc76180e9617c10efefaa5355
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2021-01-04 19:36:30 -0500

    index.ejs is showing products

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,46 @@ const products = express.Router(); //configuration
 // database with product info///
 const Product = require("../models/products.js");
 
+//Seed Route //
+
+products.get("/seed", (req, res) => {
+  Product.create(
+    [
+    {
+      name: "Shirt",
+      description:"soft, breatheable, supima cotton",
+      price: 40,
+      qty: 35,
+      img: "/images/supimacottontshirt.jpeg"
+    },
+    {
+      name: "Leather-Sandals",
+      description: "supple, calf-skin leather",
+      price: 200,
+      qty: 22,
+      img: "/images/leathersandals.jpeg"
+    },
+    {
+      name: "Cross-Body-Bag",
+      description: "nylon with metal hardware, multiple pockets",
+      price: 125,
+      qty: 50,
+      img: "/images/crossbodybag.jpg"
+    },
+    {
+      name: "Slim-Fit-Jeans",
+      description: "black, Japanese denim, slim fit",
+      price: 100,
+      qty: 15,
+      img: "/images/slimfitjeans.jpg"
+    }
+  ],
+  (error, data) => {
+    res.redirect("/products")
+  }
+  )
+})
+
 //Index products//
 
 products.get("/", (req, res) => {
