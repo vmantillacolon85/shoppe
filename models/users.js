4e09d66be483e5d4875f669117cc710016d32d53
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2021-01-06 21:56:25 -0500

    editing

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose")
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
-  usernname: { type: String, unique: true, required: true},
+  username: { type: String, unique: true, required: true},
   password: String,
 })
 
